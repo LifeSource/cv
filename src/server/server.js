2224aa406d08608156300cb255b23a85f2149883
Kenrick Wu <kenrick.wu@gmail.com>
2015-11-28 21:06:15 +0800

    Setting up for heroku.

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,8 @@ var config = require("../../config")();
 
 var app = express();
 
+var environment = config.env;
+
 // database setup
 // mongoose.connect("mongodb://localhost/<appName>")
 
@@ -16,8 +18,20 @@ app.use(bodyParser.json());
 // api goes here.
 
 // static files
-app.use(express.static(config.client));
-app.use(express.static(config.root));
-app.use("/*", express.static(config.index));
+switch (environment) {
+    case 'production':
+        console.log("**** BUILD ****");
+        app.use(express.static(config.build));
+        app.use("/*", express.static(config.build + "index.html"));
+        break;
+    default:
+        console.log("**** DEV ****");
+        app.use(express.static(config.client));
+        app.use(express.static(config.root));
+        app.use("/*", express.static(config.index));
+        break;
+}
 
-app.listen(config.port);
+app.listen(config.port, function () {
+    console.log("Server started, listening on port: " + config.port);
+});
